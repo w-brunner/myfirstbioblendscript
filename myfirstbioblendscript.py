50c1ab420537c3429266644efcd3f13eaa2c1e94
cppredstone123 <wbrunner@uchicago.edu>
2019-07-03 11:06:47 -0500

    Added parser, option to add API key

--- myfirstbioblendscript.py
+++ myfirstbioblendscript.py
@@ -1,3 +1,13 @@
 from bioblend import galaxy
+from bioblend.galaxy.objects import *
+import sys # for taking arguements
+from optparse import OptionParser # for taking options;
 
-gi = galaxy.GalaxyInstance(url = "http://localhost:8080", key = "") # gi = galaxy instance
+args=None
+
+parser = OptionParser()
+
+parser.add_option('--apikey', dest="argkey")
+options, args = parser.parse_args(args)
+
+gi = galaxy.GalaxyInstance(url = "http://localhost:8080", key = options.argkey) # gi = galaxy instance; requires an API key to "log in" via code
