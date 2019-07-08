2896b371539fea9d03adf79f52e9172eef69a5c2
cppredstone123 <wbrunner@uchicago.edu>
2019-07-08 09:20:00 -0500

    Program now lists all users in a given galaxy instance

--- myfirstbioblendscript.py
+++ myfirstbioblendscript.py
@@ -1,5 +1,6 @@
 from bioblend import galaxy
 from bioblend.galaxy.objects import *
+import bioblend
 import sys # for taking arguements
 from optparse import OptionParser # for taking options;
 
@@ -11,3 +12,8 @@ parser.add_option('--apikey', dest="argkey")
 options, args = parser.parse_args(args)
 
 gi = galaxy.GalaxyInstance(url = "http://localhost:8080", key = options.argkey) # gi = galaxy instance; requires an API key to "log in" via code
+
+allusers = gi.users.get_users()
+
+for x in range(0, len(allusers)):
+    print(allusers[x]['username'])
