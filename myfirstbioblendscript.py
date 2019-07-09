9a8aa485771be04775697e4d91a7d603c9ca79ec
cppredstone123 <wbrunner@uchicago.edu>
2019-07-09 16:39:43 -0500

    Script now collects workflows into single list of dicts, sorted by user

--- myfirstbioblendscript.py
+++ myfirstbioblendscript.py
@@ -15,11 +15,26 @@ gi = galaxy.GalaxyInstance(url = "http://localhost:8080", key = options.argkey) 
 
 allusers = gi.users.get_users()
 
+# TODO: Unify all these loops...
+
 # Create API keys for users that don't have one
 for x in range(0, len(allusers)):
-    print(allusers[x]['username'])
+    print("Username: " + allusers[x]['username'])
     if(gi.users.get_user_apikey(allusers[x]['id']) == "Not available."):
         gi.users.create_user_apikey(allusers[x]['id']) # create API key for users that don't have one
-        print(gi.users.get_user_apikey(allusers[x]['id']))
+        print("User API Key: " + gi.users.get_user_apikey(allusers[x]['id']))
+
     else:
-        print(gi.users.get_user_apikey(allusers[x]['id']))
+        print("User API Key: " + gi.users.get_user_apikey(allusers[x]['id']))
+
+# Put all galaxy instances under one dictionary
+all_apikeys = []
+all_gi = []
+for i in range(0, len(allusers)):
+    all_apikeys.append(gi.users.get_user_apikey(allusers[i]['id']))
+    all_gi.append({'username': allusers[i]['username'],  'id': gi.users.get_user_apikey(allusers[i]['id']), 'gi': galaxy.GalaxyInstance(url = "http://localhost:8080", key = all_apikeys[i])})
+print(gi.workflows.get_workflows())
+
+all_workflows = [] # All of the workflows in the enviroment, organized by user
+for j in range(0, len(all_gi)):
+    all_workflows.append({'username': allusers[j]['username'], 'workflows': all_gi[j]['gi'].workflows.get_workflows()})
