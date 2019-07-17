04279794f5adfe1548c9844714f06fe7fc34adcf
cppredstone123 <wbrunner@uchicago.edu>
2019-07-17 11:53:16 -0500

    Script can now connect to remote galaxy instance

--- myfirstbioblendscript.py
+++ myfirstbioblendscript.py
@@ -8,12 +8,13 @@ args=None
 
 parser = OptionParser()
 
-parser.add_option('--localkey', dest="localkey")
-#parser.add_option('--remotekey', dest="remotekey")
+parser.add_option('--localkey', dest = "localkey")
+parser.add_option('--remotekey', dest = "remotekey")
+parser.add_option('--remoteurl', dest = "remoteurl")
 options, args = parser.parse_args(args)
 
 gi_local = galaxy.GalaxyInstance(url = "http://localhost:8080", key = options.localkey) # gi_local = galaxy instance; requires an API key to "log in" via code
-#gi_remote = galaxy.GalaxyInstance(url = "", key = options.remotekey)
+gi_remote = galaxy.GalaxyInstance(url = options.remoteurl + ":8080", key = options.remotekey)
 
 allusers = gi_local.users.get_users()
 
@@ -41,10 +42,12 @@ all_workflows = [] # All of the workflows in the enviroment, organized by user
 for j in range(0, len(all_gi_local)):
     all_workflows.append({'username': allusers[j]['username'], 'workflows': all_gi_local[j]['gi_local'].workflows.get_workflows()})
 
-workflow_exports = []
+workflow_exports = [] # All of the workflows of the galaxy enviroment
 # TODO: Find a way to save the assc. username of each workflow
 for k in range(0, len(all_workflows)): # User
     for a in range(0, len(all_workflows[k]['workflows'])):#Workflows within users
         workflow_exports.append(gi_local.workflows.export_workflow_dict(all_workflows[k]['workflows'][a]['id']))
-        gi_local.workflows.export_workflow_to_local_path(all_workflows[k]['workflows'][a]['id'], '/home/will/Documents/galaxy_bioblend_dev/' + all_workflows[k]['workflows'][a]['owner'] + "_" + all_workflows[k]['workflows'][a]['name'], use_default_filename=False)# Append the workflow name with name of user
+        gi_local.workflows.export_workflow_to_local_path(all_workflows[k]['workflows'][a]['id'],
+        '/home/will/Documents/galaxy_bioblend_dev/' + all_workflows[k]['workflows'][a]['owner'] + "_" + all_workflows[k]['workflows'][a]['name'],
+        use_default_filename=False)# Append the workflow name with name of user
         print("Exported " + all_workflows[k]['workflows'][a]['owner'] + "\'s workflow, " + all_workflows[k]['workflows'][a]['name'])
